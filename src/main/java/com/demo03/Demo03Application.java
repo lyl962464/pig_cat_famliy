bec23d8149f45d7acba21494e79f54a5283ddb92
DELL <zsysy163@163.com>
2018-05-20 17:23:01 +0800

    第一次提交

---
+++ src/main/java/com/demo03/Demo03Application.java
@@ -0,0 +1,12 @@
+package com.demo03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo03Application.class, args);
+	}
+}
